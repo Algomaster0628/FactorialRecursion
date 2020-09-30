ba00d611d4be67bc1f926a5dc4e72025cdcb8183
PranjalKhatri <68675142+PranjalKhatri@users.noreply.github.com>
2020-09-30 20:52:11 +0530

    Update FactRecur.py
    Added comments making it beginner friendly

--- FactRecur.py
+++ FactRecur.py
@@ -1,8 +1,17 @@
 def fact(n):
   if n > 1:
+     #Factorial of n = n * (n-1) * (n-2) *....*(2) * (1)
      factorial = n*fact(n-1)
+  #Factorial of 1 and 0 is 1
   elif n == 1 or n == 0:
       return 1
   return factorial
-x = fact(4)
-print(x)
+
+#x = fact(4)
+#print(x)
+
+#Take input
+x = input(" Type a number : ")
+#Print factorial
+print(fact(int(x)))
+
